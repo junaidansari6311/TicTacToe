baf3241ccef090593e73cbcb76cf6eeb8b70ab03
junaidansari6311 <junaidansari6311@gmail.com>
2020-02-04 13:04:15 +0530

    ADDED:function for assigning X or O to player

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -4,6 +4,7 @@ declare -a board
 #variable
 rows=3
 columns=3
+player=O
 function initializeBoard () {
 	for((i=0;i<$rows;i++))
 	do
@@ -13,4 +14,14 @@ function initializeBoard () {
 		done
 	done
 }
+function assignLetter () {
+	if [ $((RANDOM%2)) -eq 0 ]
+	then
+		player=X
+	else
+		player=O
+	fi
+	echo "player = $player"
+}
 initializeBoard
+assignLetter
