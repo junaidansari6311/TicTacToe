a60fbb255a9f58d7517968b3d77c3abf786e5f52
junaidansari6311 <junaidansari6311@gmail.com>
2020-02-04 12:26:49 +0530

    ADDED:function for intializing the board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,1 +1,16 @@
+#!/bin/bash 
 echo "WELCOME TO TIC TAC TOE"
+declare -a board
+#variable
+rows=3
+columns=3
+function initializeBoard () {
+	for((i=0;i<$rows;i++))
+	do
+		for((j=0;j<$columns;j++))
+		do
+			board[$i,$j]='-'
+		done
+	done
+}
+initializeBoard
