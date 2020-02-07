7c9d23d13764668a193efea29f8f663ee8371174
junaidansari6311 <junaidansari6311@gmail.com>
2020-02-07 17:42:04 +0530

    ADDED:Introduced the computer for playing and checking tie condition

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 echo "WELCOME TO TIC TAC TOE"
 declare -A board
+#constants
+NO_OF_ROWS=3
+NO_OF_COLUMNS=3
+TOTAL_COUNT=$(($NO_OF_ROWS*$NO_OF_COLUMNS))
 #variable
 player=O
+computer=O
 count=0
+flag=0
+tieFlag=0
 function initializeBoard () {
-	for((rows=0;rows<3;rows++))
+	for((rows=0;rows<$NO_OF_ROWS;rows++))
 	do
-		for((columns=0;columns<3;columns++))
+		for((columns=0;columns<$NO_OF_COLUMNS;columns++))
 		do
 			board[$rows,$columns]="-"
 		done
@@ -17,60 +24,56 @@ function assignLetter () {
 	if [ $((RANDOM%2)) -eq 0 ]
 	then
 		player=X
+		echo "Player will play first : $player"
+		flag=1
 	else
-		player=O
-	fi
-	echo "player = $player"
-}
-function tossForFirstTurn () {
-	if [ $((RANDOM%2)) -eq 1 ]
-	then
-		echo "Player will play first"
-	else
-		echo "Player will play second"
+		computer=X
+		echo "Computer will play first : $computer"
+		flag=0
 	fi
 }
 function displayBoard () {
 	echo "----||---||----"
-	for((rows=0;rows<3;rows++))
+	for((rows=0;rows<$NO_OF_ROWS;rows++))
 	do
-		for((columns=0;columns<3;columns++))
+		for((columns=0;columns<$NO_OF_COLUMNS;columns++))
 		do
 			echo -n "| ${board[$rows,$columns]} |"
 		done
 		echo
 		echo "----||---||----"
 	done
+	echo "*-------------*--------------*"
 }
 function populateBoard () {
-	read -p "Enter the row between (0,1,2) : " row
-	read -p "Enter the column between (0,1,2) : " column
-	isCellEmpty $row $column
+	board[$1,$2]=$3
+	((count++))
+	displayBoard
+	checkWinner $3
 }
 function isCellEmpty () {
-	if [[ $1 -lt 3 && $1 -ge 0 ]] && [[ $2 -lt 3 && $2 -ge 0 ]]
+	if [[ $1 -lt $NO_OF_ROWS && $1 -ge 0 ]] && [[ $2 -lt $NO_OF_COLUMNS && $2 -ge 0 ]]
 	then
 		if [[ ${board[$1,$2]} == "-" ]]
 		then
-			board[$1,$2]=$player
-			checkWinner $player
-			((count++))
+			populateBoard $row $column $switchPlayerTurn
 		else
 			echo "Cell is not empty"
+			switchTurns
 		fi
 	else
 		echo "Invalid $1 or $2"
+		switchTurns
 	fi
 }
 function checkWinner () {
-	displayBoard
 	CountOfDiagonal=0
 	CountOfAntiDiagonal=0
-	for((rows=0;rows<3;rows++))
-   do
+	for((rows=0;rows<$NO_OF_ROWS;rows++))
+	do
 		CountOfRow=0
 		CountOfColumn=0
-		for((columns=0;columns<3;columns++))
+		for((columns=0;columns<$NO_OF_COLUMNS;columns++))
 		do
 			if [[ ${board[$rows,$columns]} == $1 ]]
 			then
@@ -88,23 +91,47 @@ function checkWinner () {
 			then
 				((CountOfAntiDiagonal++))
 			fi
-			if [[ $CountOfRow -eq 3 || $CountOfColumn -eq 3 || $CountOfDiagonal -eq 3 || $CountOfAntiDiagonal -eq 3 ]]
+			if [[ $CountOfRow -eq $NO_OF_COLUMNS || $CountOfColumn -eq $NO_OF_COLUMNS || $CountOfDiagonal -eq $NO_OF_COLUMNS || $CountOfAntiDiagonal -eq $NO_OF_COLUMNS ]]
 			then
-				echo "$1 is a winner"
+				echo "$1 won!!!"
+				tieFlag=1
 				exit
 			fi
-
-      done
-   done
+		done
+	done
+}
+function checkTie () {
+	if [[ $tieFlag -eq 0 ]]
+	then
+		echo "It's a tie!!!"
+	fi
+}
+function switchTurns () {
+	if [[ $flag -eq 1 ]]
+	then
+		echo "Player's turn!!!"
+		read -p "Enter the row between (0,1,2) : " row
+		read -p "Enter the column betwwn (0,1,2) : " column
+		switchPlayerTurn=$player
+		isCellEmpty $row $column $switchPlayerTurn
+		flag=0
+	else
+		echo "Computer's turn!!!"
+		row=$((RANDOM%$NO_OF_ROWS))
+		column=$((RANDOM%$NO_OF_COLUMNS))
+		switchPlayerTurn=$computer
+		isCellEmpty $row $column $switchPlayerTurn
+		flag=1
+	fi
 }
 function startGame () {
-	displayBoard
-	while [ $count -lt 9 ]
+	while [[ $count -lt $TOTAL_COUNT ]]
 	do
-		populateBoard
+		switchTurns
 	done
 }
 initializeBoard
 assignLetter
-tossForFirstTurn
+displayBoard
 startGame
+checkTie
